c400605f19ce8942c2556adfead5a88754e2e804
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2023-12-02 22:57:15 +0900

    [add] #9 seed雛形

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[
+  "内科",      # 1
+  "外科",      # 2
+  "整形外科",   # 3
+  "皮膚科",    # 4
+  "脳神経外科", # 5
+  "眼科",      # 6
+  "耳鼻科",    # 7
+  "小児科",    # 8
+  "歯科",      # 9
+  "接骨院"     # 10
+].each do |name|
+    HospitalLabel.create!(name: name)
+end
+
+[
+  ["サンプルクリニック", [1]],
+  ["DIC総合病院", [1, 2, 3, 4, 5]],
+  ["眼科", [6]],
+  ["耳鼻科", [7]],
+  ["小児科", [1, 8]],
+  ["歯科", [9]],
+  ["歯科クリニック", [9]],
+  ["接骨院", [10]],
+  ["休日診療所", [1]],
+  ["医院", [1]], ["医院", [1]], ["医院", [1]], ["医院", [1]], ["医院", [1]], ["医院", [1]], ["医院", [1]], ["医院", [1]], ["医院", [1]], ["医院", [1]], ["医院", [1]], ["医院", [1]]
+].each do |name, hospital_label_ids|
+  Hospital.create!({
+    name:               Gimei.last.katakana  + name,
+    access:             "〇〇駅 徒歩◆◆分",
+    url:       "http://sample-url",
+    hospital_label_ids: hospital_label_ids
+  })
+end
+
+admin.create!(
+  password:              password,
+  password_confirmation: password,
+)
