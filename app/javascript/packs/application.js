d999c77852496e774b624ac91e5aa212001cb74e
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2023-12-04 21:28:28 +0900

    [apdate] #14 modal表示成功

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+window.jQuery = window.$ = require('jquery')
 import 'bootstrap';
 import '../stylesheets/application.scss';
 
