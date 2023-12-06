dff1f25efce458000956fcf3461c4d739834e330
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2023-12-06 20:52:56 +0900

    [fix] #20 hospital_label createは成功。レンダリングが失敗

--- app/controllers/hospital_labels_controller.rb
+++ app/controllers/hospital_labels_controller.rb
@@ -3,11 +3,15 @@ class HospitalLabelsController < ApplicationController
     @q = HospitalLabel.ransack(params[:q])
     @hospital_labels = @q ? @q.result : HospitalLabel.all
     @hospital_labels = @hospital_labels.order(name: :asc).page(params[:page]).per(20)
+
+    @hospital_label = HospitalLabel.new
   end
 
   def create
     @hospital_label = HospitalLabel.create(hospital_label_params)
     @hospital_label.save ? flash[:notice] = t('notice.saved') : flash[:alert] = t('alert.false')
+
+    @hospital_labels = HospitalLabel.all
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-// window.jQuery = window.$ = require('jquery')
+import 'jquery';
 import 'bootstrap';
 import '../stylesheets/application.scss';
 
