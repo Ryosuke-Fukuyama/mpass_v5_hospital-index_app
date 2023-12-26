acde47577927476de2d47b3b22dcb1ecbbc12a77
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2023-12-26 10:41:20 +0900

    [update] #23 hospital_label 登録時ダイアログをjavascriptで実装

--- app/controllers/hospital_labels_controller.rb
+++ app/controllers/hospital_labels_controller.rb
@@ -10,6 +10,7 @@ class HospitalLabelsController < ApplicationController
   def create
     @hospital_label = HospitalLabel.create(hospital_label_params)
     @hospital_label.save
+    # 表示箇所がレンダリング範囲外の為ページ遷移後に表示されてしまう。
     # ? flash[:notice] = t('notice.saved') : flash[:alert] = t('alert.false')
 
     @hospital_labels = HospitalLabel.all.order(name: :asc).page(params[:page]).per(20)
